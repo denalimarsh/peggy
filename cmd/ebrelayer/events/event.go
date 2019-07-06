5f9bca5e3d1a2df3448a38e48e2fbf0e444a1db9
denalimarsh <denalimarsh@gmail.com>
2019-07-06 10:57:12 -0700

    Added block number to LockEvent

--- cmd/ebrelayer/events/event.go
+++ cmd/ebrelayer/events/event.go
@@ -17,16 +17,17 @@ import (
 
 // LockEvent : struct which represents a single smart contract event
 type LockEvent struct {
-	Id    [32]byte
-	From  common.Address
-	To    []byte
-	Token common.Address
-	Value *big.Int
-	Nonce *big.Int
+	Id          [32]byte
+	From        common.Address
+	To          []byte
+	Token       common.Address
+	Value       *big.Int
+	Nonce       *big.Int
+	BlockNumber uint64
 }
 
 // NewLockEvent : parses LogLock events using go-ethereum's accounts/abi library
-func NewLockEvent(contractAbi abi.ABI, eventName string, eventData []byte) LockEvent {
+func NewLockEvent(contractAbi abi.ABI, eventName string, eventData []byte, blockNumber uint64) LockEvent {
 	// Check event name
 	if eventName != "LogLock" {
 		log.Fatal("Only LogLock events are currently supported.")
@@ -38,6 +39,8 @@ func NewLockEvent(contractAbi abi.ABI, eventName string, eventData []byte) LockE
 	if err != nil {
 		log.Fatalf("Unpacking: %v", err)
 	}
+
+	event.BlockNumber = blockNumber
 
 	PrintEvent(event)
 
@@ -53,8 +56,9 @@ func PrintEvent(event LockEvent) {
 	token := event.Token.Hex()
 	value := event.Value
 	nonce := event.Nonce
+	blockNumber := event.BlockNumber
 
 	// Print the event's information
-	fmt.Printf("\nEvent ID: %v\nToken: %v\nSender: %v\nRecipient: %v\nValue: %v\nNonce: %v\n\n",
-		id, token, sender, recipient, value, nonce)
+	fmt.Printf("\nEvent ID: %v\nToken: %v\nSender: %v\nRecipient: %v\nValue: %v\nNonce: %v\nBlock Number: %v\n\n",
+		id, token, sender, recipient, value, nonce, blockNumber)
 }
