9b009248e3ac4bb7f3f36488dc199f53e34f966e
denalimarsh <denalimarsh@gmail.com>
2019-10-08 23:55:18 -0700

    Cosmos relayer lock event (successful test)

--- cmd/ebrelayer/relayer/cosmosRelayer.go
+++ cmd/ebrelayer/relayer/cosmosRelayer.go
@@ -12,28 +12,37 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 
-	// peggy "/src/github.com/cosmos/peggy/testnet-contracts/contracts"
-	// peggy "../../../testnet-contracts/contracts" // for demo
+	// TODO: Refactor solc generation of Peggy.go
 	peggy "github.com/cosmos/peggy/testnet-contracts/contracts"
 )
 
-// const peggy = "/src/github.com/cosmos/peggy/testnet-contracts/contracts"
+// TODO: These testing constants will be removed and replaced with Lock event attributes
+const (
+	WeiAmount = 100
+	// CosmosRecipient : hashed address "cosmos1gn8409qq9hnrxde37kuxwx5hrxpfpv8426szuv"
+	CosmosRecipient      = "0x636F736D6F7331676E38343039717139686E7278646533376B75787778356872787066707638343236737A7576"
+	EthereumTokenAddress = "0x0000000000000000000000000000000000000000"
+)
 
+// InitCosmosRelayer : initalizes a relayer which witnesses events on the Cosmos network and relays them to Ethereum
 func InitCosmosRelayer(peggyContractAddress common.Address, rawPrivateKey string) error {
 
+	// TODO: Parameterize the provider
 	provider := "http://localhost:7545"
 
-	// Star our ethereum client
+	// Start Ethereum client
 	client, err := ethclient.Dial(provider)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Parse private key
 	privateKey, err := crypto.HexToECDSA(rawPrivateKey)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Parse public key
 	publicKey := privateKey.Public()
 	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
 	if !ok {
@@ -41,6 +50,7 @@ func InitCosmosRelayer(peggyContractAddress common.Address, rawPrivateKey string
 	}
 
 	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+
 	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
 	if err != nil {
 		log.Fatal(err)
@@ -51,43 +61,45 @@ func InitCosmosRelayer(peggyContractAddress common.Address, rawPrivateKey string
 		log.Fatal(err)
 	}
 
+	// Set up tx signature authorization
 	auth := bind.NewKeyedTransactor(privateKey)
 	auth.Nonce = big.NewInt(int64(nonce))
-	auth.Value = big.NewInt(5)     // in wei
-	auth.GasLimit = uint64(300000) // 300,000 Gwei in units
+	auth.Value = big.NewInt(WeiAmount) // in wei
+	auth.GasLimit = uint64(300000)     // 300,000 Gwei in units
 	auth.GasPrice = gasPrice
 
-	// address := common.HexToAddress(peggyContractAddress)
+	// Initialize Peggy contract instance
 	instance, err := peggy.NewPeggy(peggyContractAddress, client)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Event parameters
 	recipient := []byte{}
-	copy(recipient[:], []byte("cosmos1gn8409qq9hnrxde37kuxwx5hrxpfpv8426szuv"))
+	copy(recipient[:], []byte(CosmosRecipient))
 
-	tokenAddressString := "0x7B95B6EC7EbD73572298cEf32Bb54FA408207359"
+	tokenAddressString := EthereumTokenAddress
 	if !common.IsHexAddress(tokenAddressString) {
 		return fmt.Errorf("Invalid contract-address: %v", tokenAddressString)
 	}
 	tokenAddress := common.HexToAddress(tokenAddressString)
 
-	amount := big.NewInt(0)
-	amount.SetBytes([]byte("50"))
+	amount := big.NewInt(WeiAmount)
 
+	// Send transaction to the instance's specified method
 	tx, err := instance.Lock(auth, recipient, tokenAddress, amount)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("tx sent: %s", tx.Hash().Hex()) // tx sent: 0x8d490e535678e9a24360e955d75b27ad307bdfb97a1dca51d0f3035dcee3e870
+	// Get the transaction receipt
+	receipt, err := client.TransactionReceipt(context.Background(), tx.Hash())
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// result, err := instance.Items(nil, key)
-	// if err != nil {
-	//     log.Fatal(err)
-	// }
-
-	// fmt.Println(string(result[:])) // "bar"
+	fmt.Println("Tx hash:", tx.Hash().Hex())
+	fmt.Println("Status:", receipt.Status, "\n")
 
 	return nil
 }
